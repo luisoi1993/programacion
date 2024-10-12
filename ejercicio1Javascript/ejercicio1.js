c612f910a1f8f65a94d7becfef04c29b94a56626
Luis <luisaguadodiego1993@gmail.com>
2024-10-13 01:55:58 +0200

    ejercicio1 javascript

---
+++ ejercicio1Javascript/ejercicio1.js
@@ -0,0 +1,19 @@
+var uno =1 ,dos=2 ,tres =3;
+
+if(uno>dos && uno> tres){
+  console.log(uno,dos,tres);
+  window.alert(uno,dos,tres);
+  console.error(uno,dos,tres);
+}
+
+if(dos>uno && dos>tres){
+  console.log(dos,uno,tres);
+  window.alert(dos,uno,tres);
+  console.error();(dos,uno,tres);
+}
+
+if(tres>uno && tres>dos){
+  console.log(tres,uno,tres);
+  window.alert(tres,uno,tres);
+  console.error(tres,uno,tres);
+}
